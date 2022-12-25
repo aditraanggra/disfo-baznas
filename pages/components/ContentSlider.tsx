adedd2ad9ce3a7db9577e90e58ec3e48dc610d89
aditraanggra <aditra.anggra@gmail.com>
2022-12-25 10:37:36 +0700

    fix react-slick issues

--- pages/components/ContentSlider.tsx
+++ pages/components/ContentSlider.tsx
@@ -1,22 +1,5 @@
-import Slider from 'react-slick'
-
 const ContentSlider = () => {
-  const setting = {
-    autoplay: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 800,
-    fade: true,
-    dots: true,
-  }
-  return (
-    <Slider className='flex flex-col overflow-x-hidden' {...setting}>
-      <div className='bg-red-500 h-64 w-3/4 '>1</div>
-      <div className='bg-red-500 h-64 w-3/4 '>2</div>
-      <div className='bg-red-500 h-64 w-3/4 '>3</div>
-      <div className='bg-red-500 h-64 w-3/4 '>4</div>
-    </Slider>
-  )
+  return <div></div>
 }
 
 export default ContentSlider
